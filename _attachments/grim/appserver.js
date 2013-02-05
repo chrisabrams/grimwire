8a8273b8a0037dab450a2e766d3599460e6b8f3d
Paul Frazee <pfrazee@gmail.com>
2013-02-04 20:55:58 -0600

    styling improvements

--- _attachments/grim/appserver.js
+++ _attachments/grim/appserver.js
@@ -219,7 +219,8 @@ Grim = (typeof Grim == 'undefined') ? {} : Grim;
 		}
 		return [
 		'<form action="httpl://app" data-output="true">',
-			'<intent class="freeze" action="http://grimwire.com/intents/freeze" draggable="true"><i class="intent icon-snowflake" title="Freeze"></i></intent>',
+			'<intent action="http://grimwire.com/intents/fix" draggable="true"><i class="intent icon-tools" title="Fix"></i></intent>',
+			'<intent action="http://grimwire.com/intents/freeze" draggable="true"><i class="intent icon-snowflake" title="Freeze"></i></intent>',
 			//'<a class="reset" target="-bottom" href="javascript:void(0)" title="Reset :TODO:"><i class="intent icon-leaf-1"></i></a>',
 			'<ul class="nav nav-pills">',
 				'<li><img src="https://developer.mozilla.org/files/3969/plain_sign_in_blue.png" /></li>',
