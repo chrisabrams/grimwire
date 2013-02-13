6990b780ad5c47ce360e268fcb9ba5029666744e
Paul Frazee <pfrazee@gmail.com>
2013-02-12 23:21:44 -0600

    simplified app/edit/text default interface

--- _attachments/grim/app/edit/text.js
+++ _attachments/grim/app/edit/text.js
@@ -4,29 +4,20 @@ app.onHttpRequest(function(request, response) {
 	var headers = Link.headerer();
 	headers.addLink('http://grimwire.com/grim/app/edit/text.js', 'http://grimwire.com/rels/src', { title:'application' });
 	Link.router(request).mpa('get', '/', /html/, function() {
+		var p = promise({body:''});
 		if (request.query.url) {
-			// edit
-			Link.navigator(request.query.url).getText()
-				.then(function(res) {
-					Link.responder(response).ok('html', headers).end([
-						'<form action="httpl://v1.pfraze.text.edit.app" method="post">',
-							'<input type="hidden" name="url" value="',request.query.url,'" />',
-							'<textarea class="input-block-level" rows="28" name="text">',
-								res.body.replace(/</g, '&lt;').replace(/>/g, '&gt;'),
-							'</textarea>',
-						'</form>'
-					].join(''));
-				})
-				.except(Link.responder(response).cb('badGateway'));
-		} else {
-			// load
+			p = Link.navigator(request.query.url).getText();
+		}
+		p.then(function(res) {
 			Link.responder(response).ok('html', headers).end([
-				'<form action="httpl://v1.pfraze.text.edit.app" method="get">',
-					'<input type="text" name="url" />',
-					'<input type="submit" name="Edit" />',
+				'<form action="httpl://v1.pfraze.text.edit.app" method="post">',
+					'<input type="hidden" name="url" value="',request.query.url,'" />',
+					'<textarea class="input-block-level" rows="15" name="text">',
+						res.body.replace(/</g, '&lt;').replace(/>/g, '&gt;'),
+					'</textarea>',
 				'</form>'
 			].join(''));
-		}
+		}).except(Link.responder(response).cb('badGateway'));
 	}).mpt('post', '/', /form\-data/, function() {
 		var contextData = request.body.parts[2].body;
 		var content = (typeof contextData != 'string') ? //
