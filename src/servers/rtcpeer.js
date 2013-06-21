9d3958966ede0b32ae005168f867fdeab792c95e
Paul Frazee <paulfrazee@goodybag.com>
2013-06-21 17:14:43 -0500

    added rtcpeerserver termination and improved organization/documentation

--- src/servers/rtcpeer.js
+++ src/servers/rtcpeer.js
@@ -20,19 +20,14 @@
 	// - `config.sigRelay`: a URI or navigator instance for a grimwire.com/rel/sse/relay
 	// - `config.initiate`: should this peer send the offer? If false, will wait for one
 	// - `chanOpenCb`: function, called when request channel is available
-	var DEBUGname = 'A';
 	function RTCPeerServer(config, chanOpenCb) {
-		// :DEBUG:
-		this.debugname = DEBUGname;
-		DEBUGname = 'B';
-
 		var self = this;
-		local.env.Server.call(this);
 		if (!config) config = {};
 		if (!config.sigRelay) throw "`config.sigRelay` is required";
-		var servers = defaultIceServers;
-		if (config.iceServers)
-			servers = config.iceServers.concat(servers); // :TODO: is concat what we want?
+		local.env.Server.call(this);
+
+		// :DEBUG:
+		this.debugname = config.initiate ? 'A' : 'B';
 
 		// hook up to sse relay
 		var signalHandler = onSigRelayMessage.bind(this);
@@ -40,10 +35,15 @@
 		this.sigRelay.subscribe({ headers: { 'last-event-id': -1 } })
 			.then(function(stream) {
 				self.state.signaling = true;
+				self.sigRelayStream = stream;
 				stream.on('message', signalHandler);
 			});
+		this.sigRelayStream = null;
 
 		// create peer connection
+		var servers = defaultIceServers;
+		if (config.iceServers)
+			servers = config.iceServers.concat(servers); // :TODO: is concat what we want?
 		this.peerConn = new webkitRTCPeerConnection(servers, peerConstraints);
 		this.peerConn.onicecandidate = onIceCandidate.bind(this);
 
@@ -61,7 +61,7 @@
 		this.__incomingResponses = {}; // only includes responses currently being received
 		this.__reqChannelBuffer = {}; // used to buffer messages that arrive out of order
 
-		// internal state flags
+		// state flags (for external reflection)
 		this.state = {
 			alive: true,
 			signaling: false,
@@ -73,86 +73,101 @@
 	window.RTCPeerServer = RTCPeerServer;
 	RTCPeerServer.prototype = Object.create(local.env.Server.prototype);
 
+	// :DEBUG:
 	RTCPeerServer.prototype.debugLog = function() {
 		var args = [this.debugname].concat([].slice.call(arguments));
 		console.debug.apply(console, args);
 	};
 
+
+	// server behaviors
+	// -
+
 	// request handler
 	RTCPeerServer.prototype.handleHttpRequest = function(request, response) {
-		var self = this;
 		this.debugLog('HANDLING REQUEST', request);
+		
 		if (request.path == '/') {
-			// self info
+			// Self resource
 			response.setHeader('link', [
 				{ href: '/', rel: 'self service via' },
 				{ href: '/{id}', rel: 'http://grimwire.com/rel/proxy' }
 				// :TODO: any links shared by the peer
 			]);
-			if (request.method == 'GET')
-				response.writeHead(200, 'ok').end(this.state);
-			else if (request.method == 'HEAD')
-				response.writeHead(200, 'ok').end();
-			else
-				response.writeHead(405, 'bad method').end();
-		} else {
-			// proxy
-			var targetUrl = decodeURIComponent(request.path.slice(1));
-			var targetUrld = local.web.parseUri(targetUrl);
-			var theirHost = targetUrld.authority ? (targetUrld.protocol + '://' + targetUrld.authority) : myHost;
-			var myHost = 'httpl://'+self.config.domain+'/';
-			var via = getViaDesc.call(this);
-
-			var req2 = new local.web.Request(request);
-			req2.url = targetUrl;
-			req2.headers.via = (req2.headers.via) ? req2.headers.via.concat(via) : [via];
-			
-			req2.stream = true;
-			this.peerDispatch(req2).always(function(res2) {
-				res2.headers.via = (res2.headers.via) ? res2.headers.via.concat(via) : [via];
-
-				if (res2.headers.link) {
-					res2.headers.link.forEach(function(link) {
-						var urld = local.web.parseUri(link.href);
-						if (!urld.host) link.href = theirHost + link.href;
-						link.href = myHost + link.href;
-					});
-				}
-
-				response.writeHead(res2.status, res2.reason, res2.headers);
-				res2.on('data', response.write.bind(response));
-				res2.on('end', response.end.bind(response));
-			});
-
-			request.on('data', req2.write.bind(req2));
-			request.on('end', req2.end.bind(req2));
+			if (request.method == 'GET') response.writeHead(200, 'ok').end(this.state);
+			else if (request.method == 'HEAD') response.writeHead(200, 'ok').end();
+			else response.writeHead(405, 'bad method').end();
+		}
+		else {
+			// Proxy resource
+			proxyRequestToPeer.call(this, request, response);
 		}
 	};
 
+	// sends a received request to the peer to be dispatched
+	function proxyRequestToPeer(request, response) {
+		var self = this;
+		var via = getViaDesc.call(this);
+		var myHost = 'httpl://'+self.config.domain+'/';
+
+		var targetUrl = decodeURIComponent(request.path.slice(1));
+		var targetUrld = local.web.parseUri(targetUrl);
+		var theirHost = targetUrld.authority ? (targetUrld.protocol + '://' + targetUrld.authority) : myHost;
+
+		// gen subsequent request
+		var req2 = new local.web.Request(request);
+		req2.url = targetUrl;
+		// add via
+		req2.headers.via = (req2.headers.via) ? req2.headers.via.concat(via) : [via];
+
+		// dispatch the request in the peer namespace
+		req2.stream = true;
+		this.peerDispatch(req2).always(function(res2) {
+
+			// update response links to include the proxy
+			if (res2.headers.link) {
+				res2.headers.link.forEach(function(link) {
+					var urld = local.web.parseUri(link.href);
+					if (!urld.host)
+						link.href = theirHost + link.href; // prepend their host if they gave relative links
+					link.href = myHost + link.href; // now prepend our host
+				});
+			}
+			// add via
+			res2.headers.via = (res2.headers.via) ? res2.headers.via.concat(via) : [via];
+
+			// pipe back
+			response.writeHead(res2.status, res2.reason, res2.headers);
+			res2.on('data', response.write.bind(response));
+			res2.on('end', response.end.bind(response));
+		});
+
+		// pipe out
+		request.on('data', req2.write.bind(req2));
+		request.on('end', req2.end.bind(req2));
+	}
+
+	// helper, used to gen the via header during proxying
 	function getViaDesc() {
 		return {
-			protocol: { name:'httpl', version:'0.4' },
+			protocol: { name: 'httpl', version: '0.4' },
 			host: this.config.domain,
 			comment: 'Grimwire/0.2'
 		};
 	}
 
 	RTCPeerServer.prototype.terminate = function() {
-		// :TODO:
-		this.state.alive = false;
-		this.state.signaling = false;
-		this.state.connected = false;
+		closePeer.call(this);
 	};
 
-	// sends a request to the peer to dispatch for us
-	RTCPeerServer.prototype.peerDispatch = function(request) {
-		var selfEnd = false, body = null;
-		if (!(request instanceof local.web.Request)) {
-			body = request.body;
-			request = new local.web.Request(request);
-			selfEnd = true;
-		}
 
+	// request channel behaviors
+	// -
+
+	// sends a request to the peer to dispatch in their namespace
+	// - `request`: local.web.Request
+	// - only behaves as if request.stream == true (no response buffering)
+	RTCPeerServer.prototype.peerDispatch = function(request) {
 		// generate ids
 		var reqid = this.__ridcounter++;
 		var resid = -reqid;
@@ -160,32 +175,29 @@
 		// track the response
 		var response_ = local.promise();
 		var response = new local.web.Response();
-		if (request.stream) {
-			// streaming, fulfill on 'headers'
-			response.on('headers', function(response) {
-				local.web.fulfillResponsePromise(response_, response);
-			});
-		} else {
-			// buffering, fulfill on 'close'
-			response.on('close', function() {
-				local.web.fulfillResponsePromise(response_, response);
-			});
-		}
+		response.on('headers', function(response) {
+			local.web.fulfillResponsePromise(response_, response);
+		});
 		this.__incomingResponses[resid] = response;
 
-		// shuttle request across
-		var reqmid = 0;
-		var chan = this.reqChannelReliable;
-		chan.send(reqid+':'+(reqmid++)+':h:'+JSON.stringify(request));
-		request.on('data', function(data) { chan.send(reqid+':'+(reqmid++)+':d:'+data); });
-		request.on('end', function() { chan.send(reqid+':'+(reqmid++)+':e'); });
-		request.on('close', function() { chan.send(reqid+':'+(reqmid++)+':c'); });
+		if (this.state.connected) {
+			var reqmid = 0; // message counter in the request stream
+			var chan = this.reqChannelReliable;
+			chan.send(reqid+':'+(reqmid++)+':h:'+JSON.stringify(request));
+			// wire up the request to pipe over
+			request.on('data', function(data) { chan.send(reqid+':'+(reqmid++)+':d:'+data); });
+			request.on('end', function() { chan.send(reqid+':'+(reqmid++)+':e'); });
+			request.on('close', function() { chan.send(reqid+':'+(reqmid++)+':c'); });
+		} else {
+			// not connected, send a 504
+			setTimeout(function() { response.writeHead(504, 'gateway timeout').end(); }, 0);
+			// ^ wait till next tick, as the dispatch is expected to be async
+		}
 
-		if (selfEnd) request.end(body);
 		return response_;
 	};
 
-	// request channel traffic handling
+	// request channel incoming traffic handling
 	// - message format: <rid>:<mid>:<message type>[:<message data>]
 	//   - rid: request/response id, used to group together messages
 	//   - mid: message id, used to guarantee arrival ordering
@@ -198,7 +210,7 @@
 	//   - 'c': close (request closed)
 	//   - *includes a message body
 	// - responses use the negated rid (request=5 -> response=-5)
-	function handleReqChannelReliableMessage(msg) {
+	function handleReqChannelIncomingMessage(msg) {
 		this.debugLog('REQ CHANNEL RELIABLE MSG', msg);
 
 		var parsedmsg = parseReqChannelMessage(msg);
@@ -206,12 +218,15 @@
 
 		ensureReqChannelOrder.call(this, parsedmsg, function() {
 			if (parsedmsg[0] > 0)
+				// received a request to be dispatched within our namespace
 				handlePeerRequest.apply(this, parsedmsg);
 			else
+				// received a response to a previous request of ours
 				handlePeerResponse.apply(this, parsedmsg);
 		});
 	}
 
+	// handles incoming request messages from the peer
 	function handlePeerRequest(reqid, mid, mtype, mdata) {
 		var chan = this.reqChannelReliable;
 		var request;
@@ -219,12 +234,14 @@
 			try { request = JSON.parse(mdata); }
 			catch (e) { return console.warn('RTCPeerServer - Unparseable request headers message from peer', reqid, mtype, mdata); }
 
+			// new request from the peer, redispatch it on their behalf
 			request.stream = true;
 			request = new local.web.Request(request);
 			local.web.dispatch(request, this).always(function(response) {
-				var resmid = 0;
 				var resid = -reqid; // indicate response with negated request id
+				var resmid = 0; // message counter in the response stream
 				chan.send(resid+':'+(resmid++)+':h:'+JSON.stringify(response));
+				// wire up the response to pipe back
 				response.on('data', function(data) { chan.send(resid+':'+(resmid++)+':d:'+data); });
 				response.on('end', function() { chan.send(resid+':'+(resmid++)+':e'); });
 				response.on('close', function() { chan.send(resid+':'+(resmid++)+':c'); });
@@ -234,10 +251,12 @@
 		} else {
 			request = this.__incomingRequests[reqid];
 			if (!request) { return console.warn('RTCPeerServer - Invalid request id', reqid, mtype, mdata); }
+			// pass on additional messages in the request stream as they come
 			switch (mtype) {
 				case 'd': request.write(mdata); break;
 				case 'e': request.end(); break;
 				case 'c':
+					// request stream closed, shut it down
 					request.close();
 					delete this.__incomingRequests[reqid];
 					delete this.__reqChannelBuffer[reqid];
@@ -247,10 +266,12 @@
 		}
 	}
 
+	// handles response messages from a previous request made to the peer
 	function handlePeerResponse(resid, mid, mtype, mdata) {
 		var response = this.__incomingResponses[resid];
 		if (!response)
 			return console.warn('RTCPeerServer - Invalid response id', resid, mtype, mdata);
+		// pass on messages in the response stream as they come
 		switch (mtype) {
 			case 'h':
 				try { mdata = JSON.parse(mdata); }
@@ -260,15 +281,16 @@
 			case 'd': response.write(mdata); break;
 			case 'e': response.end(); break;
 			case 'c':
+				// response stream closed, shut it down
 				response.close();
-				delete this.__incomingResponses[resid]; // stop tracking
+				delete this.__incomingResponses[resid];
 				delete this.__reqChannelBuffer[resid];
 				break;
 			default: console.warn('RTCPeerServer - Unrecognized message from peer', resid, mtype, mdata);
 		}
 	}
 
-	// splits the message into its parts
+	// splits a request-channel message into its parts
 	// - format: <rid>:<message type>[:<message>]
 	var reqChannelMessageRE = /([\-\d]+):([\-\d]+):(.)(:.*)?/;
 	function parseReqChannelMessage(msg) {
@@ -310,33 +332,29 @@
 		this.reqChannel.onclose = onReqChannelClose.bind(this);
 		this.reqChannel.onerror = onReqChannelError.bind(this);
 		// this.reqChannel.onmessage = handleReqChannelMessage.bind(this);
-		this.reqChannelReliable.onmessage = handleReqChannelReliableMessage.bind(this);
+		this.reqChannelReliable.onmessage = handleReqChannelIncomingMessage.bind(this);
 	}
 
 	function onReqChannelOpen(e) {
-		// :TODO:
 		this.debugLog('REQ CHANNEL OPEN', e);
 		this.state.connected = true;
 		if (typeof this.chanOpenCb == 'function')
 			this.chanOpenCb();
-		// this.reqChannel.send('Hello! from '+this.debugname);
-		// this.reqChannelReliable.send('Reliable Hello! from '+this.debugname);
 	}
 
 	function onReqChannelClose(e) {
-		// :TODO:
+		// :TODO: anything?
 		this.debugLog('REQ CHANNEL CLOSE', e);
 	}
 
 	function onReqChannelError(e) {
-		// :TODO:
+		// :TODO: anything?
 		this.debugLog('REQ CHANNEL ERR', e);
 	}
 
-	// function handleReqChannelMessage(e) {
-	// 	// :TODO:
-	// 	this.debugLog('REQ CHANNEL MSG', e);
-	// }
+
+	// signal relay behaviors
+	// -
 
 	// called when we receive a message from the relay
 	function onSigRelayMessage(m) {
@@ -354,6 +372,11 @@
 				// peer's ready to start
 				if (this.__offerOnReady)
 					sendOffer.call(this);
+				break;
+
+			case 'closed':
+				// peer's dead, shut it down
+				closePeer.call(this);
 				break;
 
 			case 'candidate':
@@ -425,6 +448,19 @@
 			null,
 			mediaConstraints
 		);
+	}
+
+	// helper shuts down session
+	function closePeer() {
+		this.signal({ type: 'closed' });
+		this.state.alive = false;
+		this.state.signaling = false;
+		this.state.connected = false;
+
+		if (this.sigRelayStream)
+			this.sigRelayStream.close();
+		if (this.peerConn)
+			this.peerConn.close();
 	}
 
 	// helper called whenever we have a remote session description
